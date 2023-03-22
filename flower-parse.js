ea3f8e63f909b179f79e570e6c4de978093eacbe
Harriet H-W <harriet.horobinworley@digital.trade.gov.uk>
2023-03-22 16:48:40 +0000

    wip add randomly generated flowers
    instead of relative position, need to be absolute but within bounds of the bed div

--- flower-parse.js
+++ flower-parse.js
@@ -1,13 +1,26 @@
-const generateFlowers = (json) => {
-    const flowerContent = '&#9880;'
-    const flowerContainer = document.createElement('div')
-    const flowerElement = document.createElement('p')
-    flowerElement.innerHTML = flowerContent
-    flowerElement.classList.add("flower__content")
-    flowerContainer.appendChild(flowerElement)
-
-    const bedElement = document.getElementById('bed')
-    bedElement.appendChild(flowerContainer)
+function randomInRange(min, max) {
+  return Math.floor(Math.random() * (max - min) + 1 + min);
 }
 
-generateFlowers()
+const generateFlowers = () => {
+  const flowerContent = "&#9880;";
+  flowerData.forEach((flower, index) => {
+    //create flower element
+    const flowerWrapper = document.createElement("div");
+    flowerWrapper.classList.add("flower__wrapper");
+    const flowerElement = document.createElement("p");
+    flowerElement.innerHTML = flowerContent;
+    flowerElement.classList.add("flower__content");
+    flowerWrapper.appendChild(flowerElement);
+
+    //add random position
+    flowerWrapper.style.left = `${randomInRange(0, 200).toString()}px`;
+    flowerWrapper.style.top = `${randomInRange(0, 400).toString()}px`; 
+
+    //add to bed
+    const bedElement = document.getElementById("bed");
+    bedElement.appendChild(flowerWrapper);
+  });
+};
+
+generateFlowers();
