452b7187987c1bbfdfccd3339877f53a5f9b9ab0
Harriet H-W <harriet.horobinworley@digital.trade.gov.uk>
2023-03-27 14:23:30 +0100

    use local data if firestore down

--- flower-data.js
+++ flower-data.js
@@ -1,234 +1,218 @@
 const flowerData = [
   {
-    "Subject": "A Jerico Rose",
-    "Body": "Plant a jerico rose as a symbol for resilience, adaptability, and endurance\nSlow growth as an act of resistance in a fast moving, disposable world\nParticipatory arts as an increasingly participated plataform for care and resistance, from local to global \n\n\n",
-    "Attachment": "",
-    "Author": "Anonymous",
+    Subject: "A Jerico Rose",
+    text: "Plant a jerico rose as a symbol for resilience, adaptability, and endurance\nSlow growth as an act of resistance in a fast moving, disposable world\nParticipatory arts as an increasingly participated plataform for care and resistance, from local to global \n\n\n",
+    author: "Anonymous",
     "Created At": "2023-03-17 16:52:13 UTC",
-    "Updated At": "2023-03-17 16:58:13 UTC"
+    "Updated At": "2023-03-17 16:58:13 UTC",
   },
   {
-    "Subject": "Gathering Garden",
-    "Body": "trellace- infrastructure- stable access for inclusion to prevent plants from withering, to support threshold and entry.\n\nsunflowers-regenerative- seeds fall and feed others. They replant themselves.\n\ncollective imagination- vision of where to plant things, where will the sun hit? Permission to replant/restructure in response to the weather.\n\nAdaptability. Holistic overview-time to think-creating blue prints for the garden.\n",
-    "Attachment": "",
-    "Author": "lottierimmer",
+    Subject: "Gathering Garden",
+    text: "trellace- infrastructure- stable access for inclusion to prevent plants from withering, to support threshold and entry.\n\nsunflowers-regenerative- seeds fall and feed others. They replant themselves.\n\ncollective imagination- vision of where to plant things, where will the sun hit? Permission to replant/restructure in response to the weather.\n\nAdaptability. Holistic overview-time to think-creating blue prints for the garden.\n",
+    author: "lottierimmer",
     "Created At": "2023-03-17 16:49:44 UTC",
-    "Updated At": "2023-03-17 16:49:44 UTC"
+    "Updated At": "2023-03-17 16:49:44 UTC",
   },
   {
-    "Subject": "",
-    "Body": "Ready to listen,\ncurious to receive and\nto fill up our cup.\n\n\n\n",
-    "Attachment": "",
-    "Author": "djunamount01",
-    "Created At": "2023-03-17 10:35:12 UTC",
-    "Updated At": "2023-03-17 10:35:12 UTC"
+    Subject: "",
+    text: "Ready to listen,\ncurious to receive and\nto fill up our cup.\n\n\n\n",
+    author: "djunamount01",
   },
   {
-    "Subject": "haiku Room 8",
-    "Body": "awaking slowly\nupwardly inspired\nbuzzing renewed\n",
-    "Attachment": "",
-    "Author": "Anonymous",
+    Subject: "haiku Room 8",
+    text: "awaking slowly\nupwardly inspired\nbuzzing renewed\n",
+
+    author: "Anonymous",
     "Created At": "2023-03-17 10:32:39 UTC",
-    "Updated At": "2023-03-17 10:33:23 UTC"
+    "Updated At": "2023-03-17 10:33:23 UTC",
   },
   {
-    "Subject": "",
-    "Body": "Creating, sharing\n\n\nSupporting radical kindness\n\n\nBuilding resilience\n \n\n\nNat + Bethan\n\n\n",
-    "Attachment": "",
-    "Author": "Anonymous",
+    Subject: "",
+    text: "Creating, sharing\n\n\nSupporting radical kindness\n\n\nBuilding resilience\n \n\n\nNat + Bethan\n\n\n",
+
+    author: "Anonymous",
     "Created At": "2023-03-17 10:29:38 UTC",
-    "Updated At": "2023-03-17 10:29:38 UTC"
+    "Updated At": "2023-03-17 10:29:38 UTC",
   },
   {
-    "Subject": "",
-    "Body": "Ready to listen,\ncurious to receive and\nto fill up our cup\n",
-    "Attachment": "",
-    "Author": "djunamount01",
+    Subject: "",
+    text: "Ready to listen,\ncurious to receive and\nto fill up our cup\n",
+
+    author: "djunamount01",
     "Created At": "2023-03-17 10:29:55 UTC",
-    "Updated At": "2023-03-17 10:29:55 UTC"
+    "Updated At": "2023-03-17 10:29:55 UTC",
   },
   {
-    "Subject": "Opening Haiku",
-    "Body": "Eager to process\nExcited to be present\nYoung People of now \n\nAllan, Abril, Sarah, Katie \n",
-    "Attachment": "",
-    "Author": "Anonymous",
+    Subject: "Opening Haiku",
+    text: "Eager to process\nExcited to be present\nYoung People of now \n\nAllan, Abril, Sarah, Katie \n",
+    author: "Anonymous",
     "Created At": "2023-03-17 10:26:40 UTC",
-    "Updated At": "2023-03-17 10:26:44 UTC"
+    "Updated At": "2023-03-17 10:26:44 UTC",
   },
   {
-    "Subject": "Opening Haiku ",
-    "Body": "We are making space\nFor whoever comes along\nTo be who you are \n\nLucy, Liz and Danielle \n",
-    "Attachment": "",
-    "Author": "gathering1",
+    Subject: "Opening Haiku ",
+    text: "We are making space\nFor whoever comes along\nTo be who you are \n\nLucy, Liz and Danielle \n",
+    author: "gathering1",
     "Created At": "2023-03-17 10:25:12 UTC",
-    "Updated At": "2023-03-17 10:25:12 UTC"
+    "Updated At": "2023-03-17 10:25:12 UTC",
   },
   {
-    "Subject": "Haiku",
-    "Body": "Interdependent\nStretching our leaves to the sun\nIn nourishing soil\n\nArdis, Juliette, Emilian\n",
-    "Attachment": "",
-    "Author": "Anonymous",
+    Subject: "Haiku",
+    text: "Interdependent\nStretching our leaves to the sun\nIn nourishing soil\n\nArdis, Juliette, Emilian\n",
+
+    author: "Anonymous",
     "Created At": "2023-03-17 10:23:16 UTC",
-    "Updated At": "2023-03-17 10:23:16 UTC"
+    "Updated At": "2023-03-17 10:23:16 UTC",
   },
   {
-    "Subject": "Closing Haiku ",
-    "Body": "Driving Compassion \nTo be the Evolution \nReparative Rest \n\n(rewritten incase not uploaded)\nRachel / Sarah \n",
-    "Attachment": "",
-    "Author": "Anonymous",
+    Subject: "Closing Haiku ",
+    text: "Driving Compassion \nTo be the Evolution \nReparative Rest \n\n(rewritten incase not uploaded)\nRachel / Sarah \n",
+
+    author: "Anonymous",
     "Created At": "2023-03-16 16:35:45 UTC",
-    "Updated At": "2023-03-16 16:35:45 UTC"
+    "Updated At": "2023-03-16 16:35:45 UTC",
   },
   {
-    "Subject": "",
-    "Body": "A messy jigsaw that doesn't always fit\n\n\nThe parts don’t always match, but still something still emerges\n\n\nIts shared and connected, through misunderstandings, listening and hard work, we join the dots\n\n\n",
-    "Attachment": "",
-    "Author": "gathering1",
+    Subject: "",
+    text: "A messy jigsaw that doesn't always fit\n\n\nThe parts don’t always match, but still something still emerges\n\n\nIts shared and connected, through misunderstandings, listening and hard work, we join the dots\n\n\n",
+
+    author: "gathering1",
     "Created At": "2023-03-16 16:34:17 UTC",
-    "Updated At": "2023-03-16 16:34:17 UTC"
+    "Updated At": "2023-03-16 16:34:17 UTC",
   },
   {
-    "Subject": "Haiku 2",
-    "Body": "Sometimes care is hard\nbeing a good ancestor\nour journey needs time\n",
-    "Attachment": "",
-    "Author": "Anonymous",
+    Subject: "Haiku 2",
+    text: "Sometimes care is hard\nbeing a good ancestor\nour journey needs time\n",
+    author: "Anonymous",
     "Created At": "2023-03-16 16:33:02 UTC",
-    "Updated At": "2023-03-16 16:33:02 UTC"
+    "Updated At": "2023-03-16 16:33:02 UTC",
   },
   {
-    "Subject": "Closing Haiku...",
-    "Body": "We came up with the first two lines! I finished the last one, it could be fun to see others final lines, feel free :D\n\nEnthusiasm\nlocation no barrier\nmeet . connect . blend . love\n\n\n",
-    "Attachment": "",
-    "Author": "Anonymous",
+    Subject: "Closing Haiku...",
+    text: "We came up with the first two lines! I finished the last one, it could be fun to see others final lines, feel free :D\n\nEnthusiasm\nlocation no barrier\nmeet . connect . blend . love\n\n\n",
+
+    author: "Anonymous",
     "Created At": "2023-03-16 16:33:40 UTC",
-    "Updated At": "2023-03-17 17:03:30 UTC"
+    "Updated At": "2023-03-17 17:03:30 UTC",
   },
   {
-    "Subject": "",
-    "Body": "connect big ideas\ninto the hyper local\na micro magic\n",
-    "Attachment": "",
-    "Author": "gatheringmomentum2023",
+    Subject: "",
+    text: "connect big ideas\ninto the hyper local\na micro magic\n",
+    author: "gatheringmomentum2023",
     "Created At": "2023-03-16 16:31:50 UTC",
-    "Updated At": "2023-03-16 16:31:54 UTC"
+    "Updated At": "2023-03-16 16:31:54 UTC",
   },
   {
-    "Subject": "closing haiku",
-    "Body": "Restful, Radical\nWe have no destination\nTruth, trust, expansion \n",
-    "Attachment": "",
-    "Author": "Emily (EmilySpargo)",
+    Subject: "closing haiku",
+    text: "Restful, Radical\nWe have no destination\nTruth, trust, expansion \n",
+    author: "Emily (EmilySpargo)",
     "Created At": "2023-03-16 16:31:33 UTC",
-    "Updated At": "2023-03-16 16:32:56 UTC"
+    "Updated At": "2023-03-16 16:32:56 UTC",
   },
   {
-    "Subject": "Haiku 2",
-    "Body": "Driving compassion\nTo be the evolution\nReparative rest\n\nby Sarah &amp; Rach\n",
-    "Attachment": "",
-    "Author": "rach66",
+    Subject: "Haiku 2",
+    text: "Driving compassion\nTo be the evolution\nReparative rest\n\nby Sarah &amp; Rach\n",
+
+    author: "rach66",
     "Created At": "2023-03-16 16:28:32 UTC",
-    "Updated At": "2023-03-16 16:28:32 UTC"
+    "Updated At": "2023-03-16 16:28:32 UTC",
   },
   {
-    "Subject": "Closing Haiku ",
-    "Body": "A messy jigsaw that doesn't always fit\n\n\nThe parts don’t always match, but still something still emerges\n\n\nIts shared and connected, through misunderstandings, listening and hard work, we join the dots\n\n\n",
-    "Attachment": "",
-    "Author": "gathering1",
+    Subject: "Closing Haiku ",
+    text: "A messy jigsaw that doesn't always fit\n\n\nThe parts don’t always match, but still something still emerges\n\n\nIts shared and connected, through misunderstandings, listening and hard work, we join the dots\n\n\n",
+
+    author: "gathering1",
     "Created At": "2023-03-16 16:26:52 UTC",
-    "Updated At": "2023-03-16 16:26:52 UTC"
+    "Updated At": "2023-03-16 16:26:52 UTC",
   },
   {
-    "Subject": "Closing Haiku ",
-    "Body": "Ears wide listening \nComforting Community\nMoving Us forward\n\nKatie, Allan, Emilian\n",
-    "Attachment": "",
-    "Author": "Anonymous",
+    Subject: "Closing Haiku ",
+    text: "Ears wide listening \nComforting Community\nMoving Us forward\n\nKatie, Allan, Emilian\n",
+    author: "Anonymous",
     "Created At": "2023-03-16 16:22:16 UTC",
-    "Updated At": "2023-03-16 16:22:48 UTC"
+    "Updated At": "2023-03-16 16:22:48 UTC",
   },
   {
-    "Subject": "Haiku",
-    "Body": "Honest generous\nHolding Vulnerability\nSharing Power \n\nJane, Euella and Fran\n\n\n",
-    "Attachment": "",
-    "Author": "Anonymous",
+    Subject: "Haiku",
+    text: "Honest generous\nHolding Vulnerability\nSharing Power \n\nJane, Euella and Fran\n\n\n",
+    author: "Anonymous",
     "Created At": "2023-03-16 12:13:13 UTC",
-    "Updated At": "2023-03-16 13:10:13 UTC"
+    "Updated At": "2023-03-16 13:10:13 UTC",
   },
   {
-    "Subject": "Haiku",
-    "Body": "Collaboration\nBeing open without fear\nYoung voices heard\n",
-    "Attachment": "",
-    "Author": "Anonymous",
+    Subject: "Haiku",
+    text: "Collaboration\nBeing open without fear\nYoung voices heard\n",
+
+    author: "Anonymous",
     "Created At": "2023-03-16 12:06:04 UTC",
-    "Updated At": "2023-03-16 13:10:14 UTC"
+    "Updated At": "2023-03-16 13:10:14 UTC",
   },
   {
-    "Subject": "Haiku",
-    "Body": "Challenge to create\nInspiration for change\nLike minded people \n\nCorrie, Ros and Shona\n",
-    "Attachment": "",
-    "Author": "Anonymous",
+    Subject: "Haiku",
+    text: "Challenge to create\nInspiration for change\nLike minded people \n\nCorrie, Ros and Shona\n",
+
+    author: "Anonymous",
     "Created At": "2023-03-16 10:59:52 UTC",
-    "Updated At": "2023-03-16 13:10:16 UTC"
+    "Updated At": "2023-03-16 13:10:16 UTC",
   },
   {
-    "Subject": "HAIKU",
-    "Body": "Connecting with you\nCreating new views\nInspiration for something new\n",
-    "Attachment": "",
-    "Author": "Anonymous",
+    Subject: "HAIKU",
+    text: "Connecting with you\nCreating new views\nInspiration for something new\n",
+    author: "Anonymous",
     "Created At": "2023-03-16 10:31:06 UTC",
-    "Updated At": "2023-03-16 13:07:09 UTC"
+    "Updated At": "2023-03-16 13:07:09 UTC",
   },
   {
-    "Subject": "",
-    "Body": "shared empowerment\ncircle of nurturing dreams\nfor learning and growth\n\nDanielle and Rhian\n",
-    "Attachment": "",
-    "Author": "Anonymous",
+    Subject: "",
+    text: "shared empowerment\ncircle of nurturing dreams\nfor learning and growth\n\nDanielle and Rhian\n",
+    author: "Anonymous",
     "Created At": "2023-03-16 10:29:47 UTC",
-    "Updated At": "2023-03-16 10:30:26 UTC"
+    "Updated At": "2023-03-16 10:30:26 UTC",
   },
   {
-    "Subject": "",
-    "Body": "Balancing perspectives\nChallenging norms and values\nActive listening\n\nC.L, Emma and Nat, Rachel\n",
-    "Attachment": "",
-    "Author": "Anonymous",
+    Subject: "",
+    text: "Balancing perspectives\nChallenging norms and values\nActive listening\n\nC.L, Emma and Nat, Rachel\n",
+    author: "Anonymous",
     "Created At": "2023-03-16 10:29:04 UTC",
-    "Updated At": "2023-03-16 13:07:07 UTC"
+    "Updated At": "2023-03-16 13:07:07 UTC",
   },
   {
-    "Subject": "Haiku",
-    "Body": "New space to connect \nThe rain brings us together\nLearn from each other\n\nArdis, Juliette, Emily\n",
-    "Attachment": "",
-    "Author": "Emily (EmilySpargo)",
+    Subject: "Haiku",
+    text: "New space to connect \nThe rain brings us together\nLearn from each other\n\nArdis, Juliette, Emily\n",
+    author: "Emily (EmilySpargo)",
     "Created At": "2023-03-16 10:26:47 UTC",
-    "Updated At": "2023-03-16 13:07:05 UTC"
+    "Updated At": "2023-03-16 13:07:05 UTC",
   },
   {
-    "Subject": "Lottie & Emilian",
-    "Body": "Clouds floating slowly,\nbirds singing, their voices loud.\nThe future is bright.\n",
-    "Attachment": "",
-    "Author": "lottierimmer",
+    Subject: "Lottie & Emilian",
+    text: "Clouds floating slowly,\nbirds singing, their voices loud.\nThe future is bright.\n",
+
+    author: "lottierimmer",
     "Created At": "2023-03-16 10:25:44 UTC",
-    "Updated At": "2023-03-16 13:10:15 UTC"
+    "Updated At": "2023-03-16 13:10:15 UTC",
   },
   {
-    "Subject": "Haiku",
-    "Body": "Active engagement\nInspiring audiences\nNext generation\n\nBy Allan &amp; Rach\n",
-    "Attachment": "",
-    "Author": "rach66",
+    Subject: "Haiku",
+    text: "Active engagement\nInspiring audiences\nNext generation\n\nBy Allan &amp; Rach\n",
+
+    author: "rach66",
     "Created At": "2023-03-16 10:22:23 UTC",
-    "Updated At": "2023-03-16 10:22:23 UTC"
+    "Updated At": "2023-03-16 10:22:23 UTC",
   },
   {
-    "Subject": "",
-    "Body": "Coming together\nWe're gathering momentum\nTo make lasting change\n",
-    "Attachment": "",
-    "Author": "Anonymous",
+    Subject: "",
+    text: "Coming together\nWe're gathering momentum\nTo make lasting change\n",
+
+    author: "Anonymous",
     "Created At": "2023-03-16 10:21:23 UTC",
-    "Updated At": "2023-03-16 10:21:23 UTC"
+    "Updated At": "2023-03-16 10:21:23 UTC",
   },
   {
-    "Subject": "",
-    "Body": "Collective Dreaming\nHope to challenge status quo\nIncluding us all  \n",
-    "Attachment": "",
-    "Author": "gatheringmomentum2023",
+    Subject: "",
+    text: "Collective Dreaming\nHope to challenge status quo\nIncluding us all  \n",
+
+    author: "gatheringmomentum2023",
     "Created At": "2023-03-16 09:26:23 UTC",
-    "Updated At": "2023-03-16 13:07:04 UTC"
-  }
-]
+    "Updated At": "2023-03-16 13:07:04 UTC",
+  },
+];
