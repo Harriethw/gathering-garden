8825bbdfe4be726a84b5f094c7857eb9bc445ac4
Harriet H-W <harriet.horobinworley@digital.trade.gov.uk>
2023-03-27 12:39:23 +0100

    initial add of firebase

--- flower-parse.js
+++ flower-parse.js
@@ -4,6 +4,16 @@ function randomInRange(min, max) {
 
 const generateFlowers = () => {
   const flowerContent = "&#9880;";
+
+  db.collection("flowers")
+    .get()
+    .then((querySnapshot) => {
+      querySnapshot.forEach((doc) => {
+        console.log(`${doc.id} => ${doc.data()}`);
+      });
+    })
+    .catch((error) => console.log(error));
+    
   flowerData.forEach((flower, index) => {
     //create flower element
     const flowerWrapper = document.createElement("div");
@@ -16,12 +26,12 @@ const generateFlowers = () => {
     //add tooltip text
     const tooltipText = document.createElement("span");
     tooltipText.classList.add("flower__text");
-    tooltipText.innerHTML = flower["Body"]
-    flowerWrapper.appendChild(tooltipText)
+    tooltipText.innerHTML = flower["Body"];
+    flowerWrapper.appendChild(tooltipText);
 
     //add random position
     flowerWrapper.style.left = `${randomInRange(0, 200).toString()}px`;
-    flowerWrapper.style.top = `${randomInRange(0, 400).toString()}px`; 
+    flowerWrapper.style.top = `${randomInRange(0, 400).toString()}px`;
 
     //add to bed
     const bedElement = document.getElementById("bed");
